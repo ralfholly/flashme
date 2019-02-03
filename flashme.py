c1dd481af185a08929f6fb6bd3d9b3f82145e780
Ralf Holly <ralf.holly@approxion.com>
2019-02-03 19:59:15 +0100

    Added operations wrong/right

--- flashme.py
+++ flashme.py
@@ -1,6 +1,42 @@
 #!/usr/bin/env python3
 
+# pylint:disable=too-few-public-methods
 class FlashCard:
-    def __init__(self, front, back):
+    def __init__(self, front, back, box=0):
         self.front = front
         self.back = back
+        self.box = box
+
+class Deck:
+    box_count = 5
+
+    def __init__(self, filename=""):
+        self.filename = filename
+        self.boxes = [[] for _ in range(Deck.box_count)]
+        self.current_box = 0
+
+    def insert_card(self, card, box):
+        if box < Deck.box_count:
+            card.box = box
+            self.boxes[box].append(card)
+
+    def restart(self):
+        self.current_box = 0
+
+    def get_next_card(self):
+        next_card = None
+        while self.current_box < Deck.box_count:
+            if self.boxes[self.current_box]:
+                next_card = self.boxes[self.current_box].pop(0)
+                break
+            self.current_box += 1
+        return next_card
+
+    def wrong(self, card):
+        card.box = 0
+        self.boxes[0].append(card)
+
+    def right(self, card):
+        if card.box < Deck.box_count - 1:
+            card.box += 1
+        self.boxes[card.box].append(card)
--- tests/test_flashme.py
+++ tests/test_flashme.py
@@ -1,7 +1,8 @@
 import unittest
 
-from flashme import FlashCard
+from flashme import FlashCard, Deck
 
+# pylint:disable=no-self-use
 class TestFlashMe(unittest.TestCase):
 
     def test_setup(self):
@@ -12,3 +13,44 @@ class TestFlashMe(unittest.TestCase):
         self.assertEqual(fc.front, "the front")
         self.assertEqual(fc.back, "the back")
 
+    def test_get_next_card(self):
+        deck = Deck()
+        deck.insert_card(FlashCard("q4", "a4"), 1)
+        deck.insert_card(FlashCard("q5", "a5"), 1)
+        deck.insert_card(FlashCard("q1", "a1"), 0)
+        deck.insert_card(FlashCard("q2", "a2"), 0)
+        deck.insert_card(FlashCard("q3", "a3"), 0)
+        deck.insert_card(FlashCard("q6", "a6"), 2)
+        self.assertEqual("q1", deck.get_next_card().front)
+        self.assertEqual("q2", deck.get_next_card().front)
+        self.assertEqual("q3", deck.get_next_card().front)
+        self.assertEqual("q4", deck.get_next_card().front)
+        self.assertEqual("q5", deck.get_next_card().front)
+        self.assertEqual("q6", deck.get_next_card().front)
+        self.assertEqual(None, deck.get_next_card())
+
+    def test_wrong(self):
+        deck = Deck()
+        deck.insert_card(FlashCard("q3", "a3"), 3)
+        card = deck.get_next_card()
+        self.assertEqual(3, card.box)
+        deck.wrong(card)
+        self.assertEqual(0, card.box)
+        deck.restart()
+        card = deck.get_next_card()
+        self.assertEqual(0, card.box)
+        self.assertEqual("q3", card.front)
+
+    def test_right(self):
+        deck = Deck()
+        deck.insert_card(FlashCard("q3", "a3"), 3)
+        card = deck.get_next_card()
+        self.assertEqual(3, card.box)
+        deck.right(card)
+        card = deck.get_next_card()
+        self.assertEqual(4, card.box)
+        self.assertEqual("q3", card.front)
+        # Cannot promote card beyond last box.
+        deck.right(card)
+        self.assertEqual(4, card.box)
+        self.assertEqual("q3", card.front)
