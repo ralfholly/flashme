68b52c94aad7ba8a3b8a92a502cc29bd917b4259
Ralf Holly <ralf.holly@approxion.com>
2019-03-02 19:13:35 +0100

    Fixed bug in flashme_notify.sh

--- utils/flashme_notify.sh
+++ utils/flashme_notify.sh
@@ -12,7 +12,7 @@ fi
 THIS_DIR=$(dirname $(readlink -f $0))
 FLASHME_EXPIRED_FILE="/tmp/flashme_expired"
 
-$THIS_DIR/flashme.sh --expired "$@" > $FLASHME_EXPIRED_FILE
+$THIS_DIR/../flashme.sh --expired "$@" > $FLASHME_EXPIRED_FILE
 if [ -s $FLASHME_EXPIRED_FILE ]; then
     notify-send "Expired Flashme decks:" "$(cat $FLASHME_EXPIRED_FILE)"
 fi
